027147ac548ff6ed118e3c30550a8ab1d8316ed8
Elgrogz <gregorgilchrist@gmail.com>
2016-12-30 17:12:12 +0000

    initial commit

---
+++ main/java/com/example/user/criminalintent/Crime.java
@@ -0,0 +1,51 @@
+package com.example.user.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by user on 30/12/2016.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+
+    }
+
+    public UUID getmID() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+
+    public void setDate(Date date) {
+        this.mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+}
